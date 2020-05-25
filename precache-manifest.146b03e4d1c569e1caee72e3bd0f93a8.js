7c971c600f65ab2581705836a0b9bb4d51e31574
admin <admin@MacBook-Pro-admin.local>
2020-05-25 13:13:08 +0300

    Updates

---
+++ precache-manifest.146b03e4d1c569e1caee72e3bd0f93a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dfc9065aab602b2b583e5e3b934ca7e",
+    "url": "/react_phone-catalog/index.html"
+  },
+  {
+    "revision": "87ec55150f4506a8466d",
+    "url": "/react_phone-catalog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e27cad47e2f73e61f88e",
+    "url": "/react_phone-catalog/static/js/2.7a861e66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_phone-catalog/static/js/2.7a861e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87ec55150f4506a8466d",
+    "url": "/react_phone-catalog/static/js/main.5f8355e1.chunk.js"
+  },
+  {
+    "revision": "e6af238f8195dfc6cde4",
+    "url": "/react_phone-catalog/static/js/runtime-main.0544dd7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_phone-catalog/static/media/logo.5d5d9eef.svg"
+  }
+]);
